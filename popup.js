05fde85c95c2e24cb9f59746624cb76f3711d2b3
ALUMNADO\juches <juches@floridauniversitaria.es>
2021-05-13 09:46:50 +0200

    13-05-21

--- popup.js
+++ popup.js
@@ -1,24 +1,28 @@
-document.addEventListener("DOMContentLoaded",ocultar);
+document.addEventListener("DOMContentLoaded",mein);
 
-function inicio(){
-var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
-	overlay = document.getElementById('overlay'),
-	popup = document.getElementById('popup'),
-	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+function mein(){
+// var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+// 	overlay = document.getElementById('overlay'),
+// 	popup = document.getElementById('popup'),
+// 	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
 
-btnAbrirPopup.addEventListener('click', function(){
-	overlay.classList.add('active');
-	popup.classList.add('active');
-});
+// btnAbrirPopup.addEventListener('click', function(){
+// 	overlay.classList.add('active');
+// 	popup.classList.add('active');
+// });
 
-btnCerrarPopup.addEventListener('click', function(e){
-	e.preventDefault();
-	overlay.classList.remove('active');
-	popup.classList.remove('active');
-});
+// btnCerrarPopup.addEventListener('click', function(e){
+// 	e.preventDefault();
+// 	overlay.classList.remove('active');
+// 	popup.classList.remove('active');
+// });
+
+
+function aranque (){
+	document.getElementById("opcioines").style.visibility='collapse';
+
 }
-
-
+aranque();
 
 
 function ocultar () {
@@ -26,18 +30,25 @@ function ocultar () {
 					let botones=document.getElementsByClassName('oculta');
 					
 					botones [0].addEventListener("click", function(){
-						  if (botones [0].innerHTML == 'Usuario') {
+						  if (botones [0].innerHTML != 'Usuario') {
 
-									botones [0].innerHTML = "Mostrar"; 
+									botones [0].innerHTML = "Usuario"; 
 
-									document.getElementById("opcioines").style.display = 'none';
+									
+									document.getElementById("opcioines").style.visibility = '';
 							}
 						  else {
-						  	botones [0].innerHTML = "Usuario"; 
+						  	
+						  	botones [0].innerHTML = "<img src='perf.png' id='perfil'>"; 
 
-									document.getElementById("opcioines").style.display = 'block';
+									document.getElementById("opcioines").style.visibility='collapse';
 						  }
 					});
 
 					
 				}
+ocultar();
+
+
+
+}
