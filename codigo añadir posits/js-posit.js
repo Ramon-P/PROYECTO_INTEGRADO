d5b28583a63681c5f0972ffca71cadf2bce08731
DESKTOP-Q05GKVF\Ramon <mazinguer5546@hotmail.com>
2021-05-23 11:02:43 +0200

    MULTIPLE CHANGES

--- codigo añadir posits/js-posit.js
+++ codigo añadir posits/js-posit.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", main);
 
 function main(){
@@ -21,3 +22,28 @@ function addElement () {
     var currentDiv = document.getElementById("div1");
     document.body.insertBefore(newDiv, currentDiv);
   }
+=======
+document.addEventListener("DOMContentLoaded", main);
+
+function main(){
+document.getElementById("boton").addEventListener("click",addElement)
+
+}
+
+function addElement () {
+    
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("class","posit")
+    var newcontenedor = document.createElement("div") ; 
+    newcontenedor.setAttribute("class","input-contenedor")
+  newDiv.appendChild(newcontenedor)
+  var newtextarea = document.createElement("textarea")
+  newtextarea.setAttribute("cols","60")
+  newtextarea.setAttribute("rows","5")
+  newcontenedor.appendChild(newtextarea)
+  
+    // añade el elemento creado y su contenido al DOM
+    var currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+  }
+>>>>>>> 092002356ded364eaf4f220d63f5058a0c750260
